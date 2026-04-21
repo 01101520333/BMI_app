b18f5659d922252cd6b81cd923ba8cdb4bc0df4a
BadrSayed2 <BadrSayed2@example.com>
2026-04-21 22:53:54 +0200

    Implement home screen layout and navigation; add custom app bar and gender selection widgets

---
+++ lib/widgets/wheigh_age_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class WheighAgeWidget extends StatelessWidget {
+  WheighAgeWidget({
+    super.key,
+    required this.wheighAgeText,
+    required this.value,
+  });
+  String wheighAgeText;
+  String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xff24263B),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+        child: Column(
+          mainAxisSize: .min,
+          children: [
+            Text(
+              wheighAgeText,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: .w300,
+                color: Color(0xff8B8C9E),
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              value,
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: .bold,
+                color: Color(0xffFFFFFF),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: .spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Color(0xff8B8C9E),
+                  shape: CircleBorder(),
+                  child: Icon(Icons.remove, color: Color(0xffFFFFFF)),
+                ),
+
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Color(0xff8B8C9E),
+                  shape: CircleBorder(),
+                  child: Icon(Icons.add, color: Color(0xffFFFFFF)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
